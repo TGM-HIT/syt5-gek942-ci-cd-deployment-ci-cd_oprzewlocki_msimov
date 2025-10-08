944abb496e36c349648aa14e339c62c8d1ae483b
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2025-10-08 10:04:38 +0200

    initial commit

---
+++ src/main/java/com/oliwier/insyrest/InsyRestApplication.java
@@ -0,0 +1,13 @@
+package com.oliwier.insyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsyRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsyRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oliwier/insyrest/InsyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliwier.insyrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsyRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
